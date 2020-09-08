6aa74e4ec83da1afa9b81c70f53e15a970417bee
Husain Ebrahim <husain@alsayed.ws>
2020-09-08 13:53:31 +0300

    added location info

--- functions/read_scanner_message.py
+++ functions/read_scanner_message.py
@@ -1,17 +1,48 @@
+import os
 import logging
 import json
 import uuid
-from datetime import datetime
+import requests
+from datetime import datetime, timedelta
+
+LOCATION = 'Undefined'
+
+
+def init():
+    """
+    Global functions of shared data between all pipelines. Used to extract the following info:
+    - Service Domain location label
+    """
+    global LOCATION
+
+    logging.info("============ Setting globals to init ============ ")
+    host = os.environ.get('UI_SVC_SERVICE_HOST', 'localhost')
+    url = f'http://{host}:30003/env'
+    r = requests.get(url)
+    if r.status_code == 200:
+        environ_list = json.loads(r.content)
+        LOCATION = environ_list.get('LOCATION', 'Undefined')
+    else:
+        LOCATION = 'Undefined'
+    return
+
+
+init()
 
 
 def main(ctx, msg):
 
+    global LOCATION
     scanner_msg = msg.decode().strip()
 
     logging.info('================ MQTT Message ===============================')    
     logging.info(f'Data from sensor: {scanner_msg}')
-    current_time = f'{datetime.utcnow().isoformat(timespec="seconds")}+00:00'
     buffer_config = ctx.get_config()
+    trigger_time = datetime.utcnow()
+    buffer_before = int(buffer_config.get('buffer_before', 5))
+    buffer_after = int(buffer_config.get('buffer_after', 15))
+    video_start_time = trigger_time - timedelta(seconds=buffer_before)
+    video_end_time = trigger_time + timedelta(seconds=buffer_after)
 
     message = {'data': {
         'operation': '',
@@ -32,9 +63,12 @@ def main(ctx, msg):
 
     message = {
         'id': str(uuid.uuid4()),
-        'trigger_time': current_time,
+        'location': LOCATION,
+        'trigger_time': f'{trigger_time.isoformat(timespec="seconds")}+00:00',
         'buffer_before': int(buffer_config.get('buffer_before', 5)),
-        'buffer_after': int(buffer_config.get('buffer_after', 15))
+        'buffer_after': int(buffer_config.get('buffer_after', 15)),
+        'video_start_time': f'{video_start_time.isoformat(timespec="seconds")}+00:00',
+        'video_end_time': f'{video_end_time.isoformat(timespec="seconds")}+00:00'
     }
         
     return ctx.send(json.dumps(message).encode())
