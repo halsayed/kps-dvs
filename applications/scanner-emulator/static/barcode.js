94c04b764acac3282fc1ee1708c51a026cd6b080
Husain Ebrahim <husain@alsayed.ws>
2020-10-01 13:20:37 +0300

    UTC timer on emulator

--- applications/scanner-emulator/static/barcode.js
+++ applications/scanner-emulator/static/barcode.js
@@ -6,3 +6,26 @@ const app = Vue.createApp({
         }
     }
 })
+
+function currentTime() {
+  var date = new Date(); /* creating object of Date class */
+  var hour = date.getUTCHours();
+  var min = date.getUTCMinutes();
+  var sec = date.getUTCSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById("clock").innerText = hour + ":" + min + ":" + sec + " - UTC"; /* adding time to the div */
+    var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
+}
+
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  }
+  else {
+    return k;
+  }
+}
+
+currentTime(); /* calling currentTime() function to initiate the process */
